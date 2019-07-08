482f7b67a4c11b7f39131fc72817a74281c6b232
Grant Pfeifer <grant.pfeifer@rovisys.com>
2019-07-08 09:20:22 -0400

    Added automatic file export

--- excelTableToMarkDown.vb
+++ excelTableToMarkDown.vb
@@ -1,36 +1,63 @@
 Sub rangeToMarkDown()
-    
+
     Dim cell As Range
     Dim selectedRange As Range
 
-    Set selectedRange = Application.Selection
+Set selectedRange = Application.Selection
 
-    Dim rowCounter As Integer
+Dim rowCounter As Integer
     Dim columnCounter As Integer
     Dim totalColumns As Integer
     Dim currentColumnWidth As Integer
+    Dim linkLength As Integer
+    Dim s As String
+    Dim n As Integer
+    Dim userName As String
+    Dim filePath As String
 
-    totalColumns = selectedRange.Columns.Count
+    Dim fileName As Variant
+    fileName = InputBox("Name for Markdown File (do not include file extension):")
+
+
+    'get username and create export file on desktop
+    userName = Environ("Username")
+    filePath = "C:\users\" + userName + "\Desktop\" + fileName + ".txt"
+
+    n = FreeFile()
+    Open filePath For Output As #n
+
+totalColumns = selectedRange.Columns.Count
 
     Dim columnWidth(50) As String    'maximum of 50 columns
 
-    '///
-    '/// init lengths of columns
-    '///
-    For i = 0 To totalColumns
-        columnWidth(i) = 0
-    Next i
 
-    '///
-    '/// go through range to calculate maximum lengths of each column
-    '///
+    'init lengths of columns
+    For I = 0 To totalColumns
+        columnWidth(I) = 0
+    Next I
+
+
+    'go through range to calculate maximum lengths of each column
     For Each Row In selectedRange.Rows
 
         columnCounter = 0
 
         For Each cell In Row.Cells
 
-            currentColumnWidth = Len(cell.Value)
+            On Error Resume Next
+            linkAddress = vbNullString
+            linkAddress = cell.Hyperlinks(1).Address
+            On Error GoTo 0
+
+            If linkAddress = vbNullString Then
+                currentColumnWidth = Len(cell.Value)
+            Else
+                temp = linkAddress
+                linkAddress = Replace(temp, " ", "%20")
+                linkLength = Len("[" & cell.Value & "]" & "(" & linkAddress & ")")
+                currentColumnWidth = linkLength
+            End If
+
 
             If (currentColumnWidth > columnWidth(columnCounter)) Then
 
@@ -39,15 +66,13 @@ Sub rangeToMarkDown()
             End If
 
             columnCounter = columnCounter + 1
-            '/// Debug.Print cell.Address, " ", cell.Value, "->", Len(cell.Value)
 
         Next cell
 
     Next Row
 
-    '///
+
     '/// go through range to calculate maximum lengths of each column
-    '///
     Dim currentLine As String
 
     rowCounter = 0
@@ -62,31 +87,49 @@ Sub rangeToMarkDown()
             currentColumnWidth = columnWidth(columnCounter)
             Dim extraSpaces As Integer
 
+
             currentLine = currentLine & " "
-            currentLine = currentLine & cell.Value
-            extraSpaces = currentColumnWidth - Len(cell.Value)
+
+            On Error Resume Next
+            linkAddress = vbNullString
+            linkAddress = cell.Hyperlinks(1).Address
+            On Error GoTo 0
+
+            If linkAddress = vbNullString Then
+                currentLine = currentLine & cell.Value
+                extraSpaces = currentColumnWidth - Len(cell.Value)
+            Else
+                temp = linkAddress
+                linkAddress = Replace(temp, " ", "%20")
+                currentLine = currentLine & "[" & cell.Value & "]" & "(" & linkAddress & ")"
+                extraSpaces = currentColumnWidth - Len("[" & cell.Value & "]" & "(" & linkAddress & ")")
+            End If
+
 
             For j = 0 To extraSpaces
 
                 currentLine = currentLine & " "
 
+
             Next j
 
-            currentLine = currentLine & " |"
+            If (columnCounter <> totalColumns - 1) Then
+                currentLine = currentLine & " |"
+            End If
 
             columnCounter = columnCounter + 1
-            '/// Debug.Print cell.Address, " ", cell.Value, "->", Len(cell.Value)
 
         Next cell
 
         Debug.Print currentLine
+    Print #n, currentLine
 
-        If (rowCounter = 0) Then
+    If (rowCounter = 0) Then
 
             currentLine = "|"
             columnCounter = 0
 
-            For j = 0 To (totalColumns - 1)
+            For j = 0 To (totalColumns - 2)
 
                 currentLine = currentLine
                 currentColumnWidth = columnWidth(columnCounter)
@@ -101,15 +144,28 @@ Sub rangeToMarkDown()
                 columnCounter = columnCounter + 1
 
             Next j
-    
+            currentColumnWidth = columnWidth(columnCounter)
+            currentLine = currentLine & "-"
+
+            For k = 0 To currentColumnWidth
+
+                currentLine = currentLine & "-"
+            Next k
+
+            currentLine = currentLine & "-"
+            columnCounter = columnCounter + 1
+
+
             Debug.Print currentLine
-        End If
+        Print #n, currentLine
+    End If
 
         rowCounter = rowCounter + 1
 
     Next Row
 
+    Close #n
 
-
+MsgBox "File saved to " + filePath
 
 End Sub
